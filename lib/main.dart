97fa2c0a144d857c1f066f72755b10e5f0014050
ozan <ozan@ozans-MacBook-Pro.local>
2020-05-06 21:42:53 -0400

    calling another stl widget as home

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       title: 'my lil app',
-       home:Scaffold(
-         appBar:AppBar(title:Text("hey app")),
-         body:Center(child:Text("hello world!")),
-       )
+       title: 'my bs app',
+       theme:ThemeData(primarySwatch: Colors.green),
+       home:HomeScreen(title: 'home page'),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final String title;      //stateless widgets are immutable thus all the data should be final 
+  HomeScreen({Key key,this.title}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(title:Text("my lil app")),
+      body:Center(child:Text("hello world!"))
     );
   }
 }
